cfd31da701a203a2307cec686d8e4736628c93c7
Angelo Geulin <angelogeulin123@gmail.com>
2023-08-15 11:39:19 +0800

    fix: add page lang and meta description

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <script setup>
 useHead({
   title: "Angelo Geulin",
+  htmlAttrs: {
+    lang: "en",
+  },
+  meta: [
+    {
+      name: "description",
+      content:
+        "I'm Angelo Geulin and this is my personal website. I'm a software hobbyist and student developer. I like tinkering with software and learning new things everyday.",
+    },
+  ],
 })
 </script>
 
